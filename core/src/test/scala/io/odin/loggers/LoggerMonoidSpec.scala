9ca7220da5d36be2e19bf93cdc739505d7dd60de
Yuri Ankudinov <stremlenye@gmail.com>
2020-11-02 17:43:18 +0100

    Adds test to demonstrate the compositiopn problem

--- core/src/test/scala/io/odin/loggers/LoggerMonoidSpec.scala
+++ core/src/test/scala/io/odin/loggers/LoggerMonoidSpec.scala
@@ -24,7 +24,7 @@ class LoggerMonoidSpec extends OdinSpec {
     }
   }
 
-  it should "(logger1 |+| logger2).log(list) <-> (logger1.log |+| logger2.log(list))" in {
+  it should "(logger1 |+| logger2).log(list) <-> (logger1.log(list) |+| logger2.log(list))" in {
     forAll { (uuid1: UUID, uuid2: UUID, msg: List[LoggerMessage]) =>
       val logger1: Logger[F] = NamedLogger(uuid1)
       val logger2: Logger[F] = NamedLogger(uuid2)
@@ -41,6 +41,40 @@ class LoggerMonoidSpec extends OdinSpec {
       val a = (logger1 |+| logger2).withMinimalLevel(level).log(msg)
       val b = (logger1.withMinimalLevel(level) |+| logger2.withMinimalLevel(level)).log(msg)
       a.written.unsafeRunSync() shouldBe b.written.unsafeRunSync()
+    }
+  }
+
+  it should "keep the minimal level for underlying loggers if not explicitly set" in {
+    forAll { (uuid1: UUID, uuid2: UUID, level: Level, msg: List[LoggerMessage]) =>
+      val logger1: Logger[F] = NamedLogger(uuid1)
+      val logger2: Logger[F] = NamedLogger(uuid2)
+      val a = (logger1.withMinimalLevel(level) |+| logger2).log(msg)
+      val b = (logger1 |+| logger2.withMinimalLevel(level)).log(msg)
+      a.written.unsafeRunSync() shouldBe b.written.unsafeRunSync()
+    }
+  }
+
+  it should "keep the minimal level for underlying loggers if not explicitly set during the further compositions" in {
+    forAll { (uuid1: UUID, uuid2: UUID, msg: List[LoggerMessage]) =>
+      import io.odin.syntax._
+      val logger1: Logger[F] = NamedLogger(uuid1).withMinimalLevel(Level.Error)
+      val logger2: Logger[F] = NamedLogger(uuid2).withMinimalLevel(Level.Info)
+      val infoMessages = msg.map(_.copy(level = Level.Error)) ++ msg.map(_.copy(level = Level.Info))
+      val a = logger1
+        .withConstContext(Map.empty)
+        .log(infoMessages)
+        .written
+        .unsafeRunSync() ++
+        logger2
+          .withConstContext(Map.empty)
+          .log(infoMessages)
+          .written
+          .unsafeRunSync()
+
+      val composedLogger = (logger1 |+| logger2).withConstContext(Map.empty)
+
+      val b = infoMessages.traverse(composedLogger.log)
+      a shouldBe b.written.unsafeRunSync()
     }
   }
 
